6cf28e3931d85fac82b9328049377e5d4522f91d
root <86170140@qq.com>
2014-12-22 17:12:39 +0800

    说的 Signed-off-by: root <86170140@qq.com>

--- advanced/backend/controllers/StoreController.php
+++ advanced/backend/controllers/StoreController.php
@@ -7,6 +7,12 @@ use yii\web\Controller;
 use common\models\LoginForm;
 use yii\filters\VerbFilter;
 
+use app\models\Shop;
+use yii\data\Pagination;
+use app\models\Control;
+use yii\db\ActiveRecord;
+
+
 
 class StoreController extends Controller
 {
@@ -16,9 +22,73 @@ class StoreController extends Controller
     //店铺管理
     //店铺列表
     public function actionStore_list(){
-        return $this->renderPartial("store_list");
+
+        if(!empty($_GET['search_id'])||!empty($_GET['search_title'])){
+       $ord='asc';
+       $id=$_GET['search_id'];
+       $title=$_GET['search_title'];
+       $where='';
+       $where.='is_close=1';
+       if(!empty($_GET['search_id'])){
+       $where.=" and shop_id = ".$id."";
+       }
+        if(!empty($_GET['search_title'])){
+       $where.=" and shop_name like '%".$title."%'";
+        }
+       $page_size=isset($_GET['pagesize'])?$_GET['pagesize']:10;
+       $data = Shop::find()->where($where); 
+       }else{
+           $page_size=isset($_GET['pagesize'])?$_GET['pagesize']:10;
+           $where='';
+           $where.='is_close=1';
+           $ord=isset($_GET['ord'])?$_GET['ord']:'asc';
+            if(!empty($_GET['ord'])){
+                $where.=" order by shop_id ".$_GET['ord']."";
+            }
+            $data = Shop::find()->where($where); 
+       }
+       $total=$data->count();
+       $pages = new Pagination(['totalCount' =>$total, 'pageSize' =>$page_size ]);
+       $list = $data->offset($pages->offset)->limit($pages->limit)->all();
+       return $this->renderPartial('store_list',[
+             'list' => $list,
+             'pages' => $pages,
+             'pagesize'=>$page_size,
+             'ord'=>$ord,
+       ]);
     }
+    //修改状态
+    public function actionUp_status(){
+        $id=$_GET['id'];
+        $stu=$_GET['stu'];
+        //echo $id.$stu;
+        $user = Shop::findone($id);
+        $user->shop_status=$stu;
+         if($user->save()) {
+            $this->redirect("?r=store/store_list");
+        }
+    }
+    //批量删除
+    public function actionDel_shop(){
+         $id=$_GET['id'];
+         $count = Shop::deleteAll("shop_id in ($id)");
+         if($count>0){
+          $this->redirect("?r=store/store_list");
+        }else{
+           $this->redirect("?r=store/store_list");
+        }
+    }
+    //编辑模式
+    public function actionUp_shop(){
+       $shop_id=$_GET['id'];
+        $list=Shop::findOne($shop_id); 
+        return $this->renderPartial("store_up",[
+             'list' => $list,
+       ]);
+    }
+
     
+
     /*
      * 威客作品
      */
--- advanced/backend/views/store/store_list.php
+++ advanced/backend/views/store/store_list.php
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+<?php
+use yii\widgets\LinkPager;
+?>
+=======
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -38,18 +44,32 @@
  <tbody>
  	<tr>
  		<th>ID</th>
+<<<<<<< HEAD
+<td><input type="text" class="txt" name="txt_shop_id" value="" id='ida' onkeyup="clearstr(this);"></td>
+<th>店铺名称</th>
+<td><input type="text" class="txt" name='txt_name' value=""  id='title' onkeyup="clearspecial(this);"></td>
+=======
 <td><input type="text" class="txt" name="txt_shop_id" value="" onkeyup="clearstr(this);"></td>
 <th>店铺名称</th>
 <td><input type="text" class="txt" name='txt_name' value="" onkeyup="clearspecial(this);"></td>
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
  	</tr>
 
 <tr>
 <th>显示条数</th>
 <td>
+<<<<<<< HEAD
+<select name="page_size" onchange="pageJump(value)">
+     <option value="10" <?php if($pagesize==10){echo "selected";}?>>每页显示10条</option>
+     <option value="5" <?php if($pagesize==5){echo "selected"; }?>>每页显示5条</option>
+     <option value="3" <?php if($pagesize==3){echo "selected"; }?>>每页显示3条</option>
+
+=======
 <select name="wh[page_size]" class="ps vm">
 <option value="10" selected="selected">每页显示10</option>
 <option value="20" >每页显示20</option>
 <option value="30" >每页显示30</option>
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 </select>
 </td>
 <th>结果排序</th>
@@ -58,11 +78,19 @@
                            <option value="shop_id"  selected="selected">默认排序</option>
                            <option value="on_time" >申请时间</option>
                       </select>
+<<<<<<< HEAD
+                      <select name="ord[]" onchange="orderJump(this.value)">
+                                <option  value="asc"  <?php if($ord=='asc'){echo "selected";}?>>递增</option>
+                                <option  value="desc"  <?php if($ord=='desc'){echo "selected";}?>>递减</option>
+                                </select>
+<button type="button" name="sbt_search" value=搜索 class="pill"  onclick="search_task()" />
+=======
                       <select name="w[ord][]">
                             <option selected="selected"  value="desc">递减</option>
                             <option  value="asc">递增</option>
                       </select>
 <button type="submit" name="sbt_search" value=搜索 class="pill" />
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 <span class="icon magnifier">&nbsp;</span>搜索</button>
 </td>
 </tr>
@@ -89,6 +117,46 @@
             <th width="15%">状态</th>
             <th width="20%">操作</th>
 </tr>
+<<<<<<< HEAD
+                     <?php             foreach($list as $key=>$val)
+      {?>
+        <tr class="item">
+        	<td><input type="checkbox" name="ckb" class="checkbox" value="<?php echo $val['shop_id']?>"><?php echo $val['shop_id']?></td>
+<td><?php echo $val['username']?></td>
+           <td><?php echo $val['shop_name']?></td>
+           <td><?php echo date("Y-m-d",$val['on_time'])?></td>
+           <td>
+               <?php if($val['shop_status']==2){ ?>
+                       <span> 待审核</span>          
+
+                    <?php }else  if($val['shop_status']==0){ ?>
+                      <span> 关闭</span>
+                   <?php }else  if($val['shop_status']==1){?>
+                      <span> 开启</span>
+                   <?php }else  if($val['shop_status']==3){?>
+                      <span> 审核未通过</span>
+                   <?php } ?>
+           </td>
+<td>
+     <?php    if($val['shop_status']==2){?>
+                        <a href="index.php?r=store/up_status&id=<?php echo $val['shop_id'] ?>&stu=0" class="button"><span class="check icon"></span>通过审核</a>
+                        <a href="index.php?r=store/up_status&id=<?php echo $val['shop_id'] ?>&stu=3" class="button">审核不通过</a>
+                        <a href="index.php?r=store/up_shop&id=<?php echo $val['shop_id'] ?>" class="button"><span class="pen icon"></span>查看</a>
+                   <?php }else  if($val['shop_status']==0){?>
+                       <a href="index.php?r=store/up_status&id=<?php echo $val['shop_id'] ?>&stu=1" class="button"><span class="pen icon"></span>开启</a>
+                       <a href="index.php?r=store/up_shop&id=<?php echo $val['shop_id'] ?>" class="button"><span class="pen icon"></span>查看</a>
+                  <?php }else  if($val['shop_status']==1){?>
+                       <a href="index.php?r=store/up_status&id=<?php echo $val['shop_id'] ?>&stu=0" class="button"><span class="pen icon"></span>关闭</a>
+                       <a href="index.php?r=store/up_shop&id=<?php echo $val['shop_id'] ?>" class="button"><span class="pen icon"></span>查看</a>
+                   <?php }else  if($val['shop_status']==3){?>
+                      <a href="index.php?r=store/up_shop&id=<?php echo $val['shop_id'] ?>" class="button"><span class="pen icon"></span>查看</a>
+                   <?php } ?>
+                       
+
+</td>
+          </tr>
+       <?php }?>          
+=======
         <tr class="item">
         	<td><input type="checkbox" name="ckb[]" class="checkbox" value="8">8</td>
 <td>mxc123</td>
@@ -195,19 +263,28 @@
 <a href="index.php?do=store&view=list&txt_username=&txt_shop_id=&page=1&w[ord][0]=&w[ord][1]=&wh[page_size]=10&ac=close&shop_id=1" class="button"><span class="pen icon"></span>关闭</a>
 </td>
           </tr>
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
            <tr>
             <td colspan="7">
             	<label for="checkbox">  
 全选</label>
 <input type="hidden" name="sbt_action" class="sbt_action"/>　
+<<<<<<< HEAD
+<button type="submit" name="sbt_action" value="批量删除" class="negative pill button" onclick="return batch_act(this,'index.php?r=store/del_shop')" >
+=======
 <button type="submit" name="sbt_action" value="批量删除" class="negative pill button" onclick="return batch_act(this,'frm_list')" >
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 <span class="trash icon"></span>批量删除</button>
 <button type="submit" name="sbt_action" value="批量审核" class="pill negative" onclick="return batch_act(this,'frm_list');" >
 <span class="lock icon"></span>批量审核</button>
 </td>
   </tr>
         </table>
+<<<<<<< HEAD
+<div class="page"><?= LinkPager::widget(['pagination' => $pages]); ?></div>
+=======
 <div class="page"></div>
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 </div>
 </div>
 </form>
@@ -313,17 +390,53 @@ function batch_act(obj, frm) {
 d = art.dialog;
 var frm = frm;
 var c = $(obj).val();
+<<<<<<< HEAD
+var conf = $(":checkbox[name='ckb']:checked").length;
+if (conf > 0) {
+d.confirm("确定" + c + '?', function() {
+var conf = $(":checkbox[name='ckb']:checked").length;
+var ida = document.getElementsByName("ckb");
+var idarr=new Array();
+		var flag=0;
+		for (var i=0; i<conf; i++)
+		{
+			if(ida[i].checked==true)
+			{
+				idarr[flag]=ida[i].value;
+				flag++;
+			}
+		}
+ location.href="index.php?r=store/del_shop&id="+idarr;
+=======
 var conf = $(":checkbox[name='ckb[]']:checked").length;
 if (conf > 0) {
 d.confirm("确定" + c + '?', function() {
 $(".sbt_action").val(c);
 $("#" + frm).submit();
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 });
 } else {
 d.alert("您没有选择任何操作项");
 }
 return false;
 }
+<<<<<<< HEAD
+function pageJump(value){
+location.href="index.php?r=store/store_list&pagesize="+value;
+}
+function orderJump(value){
+location.href="index.php?r=store/store_list&ord="+value;
+}
+function search_task(){
+    var id= document.getElementById("ida").value;
+    var title= document.getElementById("title").value;
+    location.href="index.php?r=store/store_list&search_id="+id+"&search_title="+title;
+}
 </script>
+
+
+=======
+</script>
+>>>>>>> 0f7b2d687c3f2ea5167cbd384bcd5927e552cca2
 </body>
 </html>
